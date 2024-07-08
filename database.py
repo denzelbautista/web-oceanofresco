4fb5c0510e9532a348051aa24d5a05df58081d05
denzelbautista <denzel.bautista@utec.edu.pe>
2024-07-08 11:58:12 -0500

    cred

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mC6:6AjqaC)jm}a>psMLD9I6)fw6@database-1.crikozfwdebe.us-east-1.rds.amazonaws.com:5432/oceanodb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:X|inMUV7ejbpS7w0$L)w<]zeVhir@database-1.crikozfwdebe.us-east-1.rds.amazonaws.com:5432/oceanodb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
